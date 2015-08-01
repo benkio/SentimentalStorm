09cc0530d2c7418557d087bb40ced0348bef77e3
Enrico Benini <benkio89@gmail.com>
2015-08-01 18:08:41 +0200

    Fetch first public tweet from twitter streaming API

--- src/main/scala/TwitterAPIUtils/TwitterAPIConnector.scala
+++ src/main/scala/TwitterAPIUtils/TwitterAPIConnector.scala
@@ -1,6 +1,6 @@
 package main.scala.TwitterAPIUtils
 
-import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.{TimeUnit, LinkedBlockingQueue}
 
 import com.twitter.hbc.ClientBuilder
 import com.twitter.hbc.core.Constants
@@ -8,23 +8,24 @@ import com.twitter.hbc.core.endpoint.StatusesSampleEndpoint
 import com.twitter.hbc.core.processor.StringDelimitedProcessor
 import com.twitter.hbc.httpclient.BasicClient
 
-class TwitterAPIConnector (){
-  val blockingQueue = new LinkedBlockingQueue<String>()
+class TwitterAPIConnector{
+  val blockingQueue = new LinkedBlockingQueue[String]()
   val endpoint = new StatusesSampleEndpoint()
   endpoint.stallWarnings(false)
 
-  //Create the client for connection
-  var client: BasicClient
-  def TwitterAPIConnector(consumerKey: String, consumerSecret: String, token: String, secret: String){
-    client = new ClientBuilder().name("client")
+  var client: BasicClient = null
+  def this(consumerKey: String, consumerSecret: String, token: String, secret: String) {
+    this()
+    this.client = new ClientBuilder().name("client")
             .hosts(Constants.STREAM_HOST)
             .endpoint(endpoint)
             .authentication(Auth.OAuth1(consumerKey,consumerSecret,token,secret))
             .processor(new StringDelimitedProcessor(blockingQueue))
             .build()
   }
-  def TwitterAPIConnector(username: String, password: String) {
-    client = new ClientBuilder().name("client")
+  def this(username: String, password: String) {
+    this()
+    this.client = new ClientBuilder().name("client")
             .hosts(Constants.STREAM_HOST)
             .endpoint(endpoint)
             .authentication(Auth.BasicAuth(username,password))
@@ -34,4 +35,15 @@ class TwitterAPIConnector (){
   def connectClient = client.connect()
   def stopClient = client.stop()
 
+  def popMessage : String = client.isDone() match {
+    case true => {
+      System.out.println("Client connection closed unexpectedly: " + client.getExitEvent().getMessage())
+      ""
+    }
+    case false => blockingQueue poll(5, TimeUnit.SECONDS) match {
+      case null => System.out.println("Did not receive a message in 5 seconds")
+        ""
+      case v =>  v
+    }   
+  }
 }
