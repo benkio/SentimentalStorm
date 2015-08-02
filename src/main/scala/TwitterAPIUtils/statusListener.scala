be17a0207c0fd4f25e3085dd7181ab1f3227d490
Enrico Benini <benkio89@gmail.com>
2015-08-02 15:54:33 +0200

    Update Dependencies, change twitter library to twitter4j add first spout

---
+++ src/main/scala/TwitterAPIUtils/statusListener.scala
@@ -0,0 +1,21 @@
+package TwitterAPIUtils
+
+import twitter4j.Status
+import java.util.concurrent.LinkedBlockingQueue
+import twitter4j._
+import twitter4j.StatusDeletionNotice
+
+/**
+ * Created by benkio on 02/08/15.
+ */
+class statusListener(queue: LinkedBlockingQueue[Status]) extends StatusListener{
+  override def onStatus(status:Status) {
+    queue.offer(status)
+  }
+
+  override def onDeletionNotice(sdn: StatusDeletionNotice) {}
+  override def onTrackLimitationNotice(i: Int) {}
+  override def onScrubGeo(l: Long, l1:Long) {}
+  override def onException(ex: Exception) {}
+  override def onStallWarning(arg0: StallWarning) {}
+}
---
+++ src/main/scala/spout/TwitterSpout.scala
@@ -0,0 +1,73 @@
+package spout
+
+import java.util
+import java.util.Map
+import java.util.concurrent.LinkedBlockingQueue
+
+import twitter4j._
+
+import backtype.storm.Config
+import backtype.storm.spout.SpoutOutputCollector
+import backtype.storm.task.TopologyContext
+import backtype.storm.topology.OutputFieldsDeclarer
+import backtype.storm.topology.base.BaseRichSpout
+import backtype.storm.tuple.Fields
+import backtype.storm.tuple.Values
+import twitter4j.auth.AccessToken
+import twitter4j.conf.ConfigurationBuilder
+
+import TwitterAPIUtils._
+
+class TwitterSampleSpout(keyWords: Array[String]) extends BaseRichSpout {
+
+  var _collector: SpoutOutputCollector = null
+  var queue: LinkedBlockingQueue[Status] = null
+  var _twitterStream: TwitterStream = null
+
+
+  override def open(conf: Map[_,_], context: TopologyContext, collector: SpoutOutputCollector) {
+    queue = new LinkedBlockingQueue[Status](1000)
+    _collector = collector
+
+
+
+    val twitterStream = new TwitterStreamFactory(
+      new ConfigurationBuilder().setJSONStoreEnabled(true).build())
+      .getInstance()
+
+    twitterStream.addListener(new statusListener(queue))
+    twitterStream.setOAuthConsumer(keys.APIKey, keys.APISecret)
+    twitterStream.setOAuthAccessToken(new AccessToken(keys.Token, keys.TokenSecret))
+
+    if (keyWords.length == 0)
+      twitterStream.sample()
+    else
+      twitterStream.filter(new FilterQuery().track(keyWords))
+
+  }
+
+  override def nextTuple = {
+    val ret = queue.poll()
+    if (ret != null)
+      _collector.emit(new Values(ret))
+  }
+
+  override def close = {
+    _twitterStream.shutdown()
+  }
+
+  override def getComponentConfiguration: util.Map[String, Object] = {
+    val ret = new Config
+    ret.setMaxTaskParallelism(1)
+    ret
+  }
+
+  override def ack(id: Object) { }
+
+  override def fail(id: Object) { }
+
+  override def declareOutputFields(declarer: OutputFieldsDeclarer) {
+    declarer.declare(new Fields("tweet"))
+  }
+
+}
